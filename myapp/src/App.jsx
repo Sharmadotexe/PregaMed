0b89fc10052a170f38274b2ad2336a0b98ebdd41
IshanFTW <ishanjindal2003@gmail.com>
2024-03-07 16:28:47 +0530

    added routes

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import Nutrition from './components/Nutrition'
+import Exercise from './components/Exercise'
+import Community from './components/Community'
+import Appointment from './components/Appointment'
+import Baby from './components/Baby'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
 
-const App = () => {
+function App() {
+ 
+
   return (
-    <div>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      
+        <Route path="/nutrition" element={<Nutrition />} />
+        <Route path="/exercise" element={<Exercise />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/baby" element={<Baby />} />
+        
+      
+    </Routes>
+  </BrowserRouter>
   )
 }
 
