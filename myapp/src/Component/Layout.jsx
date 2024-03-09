ed3be829c8322095d650d36adf8370128f869411
IshanFTW <ishanjindal2003@gmail.com>
2024-03-09 14:23:25 +0530

    without dropdowm error code

--- myapp/src/Component/Layout.jsx
+++ myapp/src/Component/Layout.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { Link, Outlet } from "react-router-dom"
 import Navbar from '../Pages/Navbar'
 import Dropdown from './Dropdown'
+import NavContainer from './NavContainer'
 
 function Layout() {
   return (
     <div>
+      <NavContainer/>
+      <Dropdown/>
     </div>
 
   )
