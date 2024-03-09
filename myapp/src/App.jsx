a5cd28e8563ec966779a2ea6646a4fe85db590aa
sharmadotexe <rockvikas300@gmail.com>
2024-03-09 14:02:24 +0530

    Created NavContainer Component and SignIn SignUp Page

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -8,12 +8,15 @@ import Baby from './Pages/Baby'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './Component/Layout'
 import "./index.css"
+import Register from './Component/Register'
+import Sign from './Component/Sign'
 
 
 function App() {
  
 
   return (
+<div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
     <BrowserRouter>
     <Routes>
       <Route element= {<Navbar />}>
@@ -23,12 +26,12 @@ function App() {
         <Route path="/community" element={<Community />} />
         <Route path="/appointment" element={<Appointment />} />
         <Route path="/baby" element={<Baby />} />
+        <Route path="/login" element={<Register />} />
+        <Route path="/register" element={<Sign />} />
       </Route>
-        
-        
-      
     </Routes>
   </BrowserRouter>
+  </div>
   )
 }
 
--- myapp/src/Component/Layout.jsx
+++ myapp/src/Component/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link, Outlet } from "react-router-dom"
 import Navbar from '../Pages/Navbar'
+import NavContainer from './NavContainer'
 
 function Layout() {
   return (
-    <div>
-        this is a home page
+    <div> 
+       
     </div>
 
   )
